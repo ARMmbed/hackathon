baa45822a2038c92e1b37a8cf84559a52a1d26ee
Laura <lt370@cam.ac.uk>
2015-10-25 12:12:30 +0000

    PResentation and code

---
+++ Snuzz-Final/Snuzz-Android/mobile/src/main/java/com/example/charlie/snuzz/TimePickerFragment.java
@@ -0,0 +1,42 @@
+package com.example.charlie.snuzz;
+
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.app.DialogFragment;
+import android.app.Dialog;
+import java.util.Calendar;
+import android.widget.TimePicker;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        //Use the current time as the default values for the time picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        //Create and return a new instance of TimePickerDialog
+        return new TimePickerDialog(getActivity(),this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    //onTimeSet() callback method
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute){
+        //Do something with the user chosen time
+        //Get reference of host activity (XML Layout File) TextView widget
+        TextView tv = (TextView) getActivity().findViewById(R.id.tv);
+
+        //Display the user changed time on TextView
+        tv.setText(tv.getText()+ "Alarm is set for " + String.valueOf(hourOfDay)
+                + ":" + String.valueOf(minute) + "\n");
+    }
+}
---
+++ Snuzz-Final/Snuzz-Android/wear/src/main/java/com/example/charlie/snuzz/BleComms.java
@@ -0,0 +1,14 @@
+package com.example.charlie.snuzz;
+
+/**
+ * Created by Charlie on 2015-10-24.
+ *
+ * Bluetooth communications class for Snuzz
+ *
+ * Handles packet format for the app
+ */
+public class BleComms {
+
+
+
+}
